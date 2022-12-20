22189f3d0d4bd669f2610d5c0d1f66087e430905
usamakh9 <66205267+usamakh9@users.noreply.github.com>
2022-12-20 15:47:28 +0500

    Update pdfmerger.py

--- pdfmerger.py
+++ pdfmerger.py
@@ -6,7 +6,7 @@ merger = PyPDF2.PdfFileMerger()
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
         merger.append(file)
-merger.write('Newfolder/combined.pdf')
+merger.write('combined.pdf')
 merger.close()
     
 
